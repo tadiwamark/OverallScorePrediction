76eb656599d077aeadc301b24d2052b96d53cfa9
tadiwamark <64325379+tadiwamark@users.noreply.github.com>
2021-12-01 00:35:24 +0200

    Add files via upload

---
+++ streamlitapp.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from utils import *
+
+st.write(
+    """ 
+# Overall Score Predictor 
+"""
+)
+
+joined = st.number_input("Enter  joined:")
+power_jumping = st.number_input("Enter  power_jumping:")
+power_stamina = st.number_input("Enter  power_stamina:")
+passing = st.number_input("Enter  passing:")
+team_jersey_number = st.number_input("Enter the team_jersey_number:- ")
+defending = st.number_input("Enter defending:-")
+shooting = st.number_input("Enter shooting:-")
+attacking_heading_accuracy = st.number_input(
+    "Enter attacking_heading_accuracy:-")
+height = st.number_input("Enter your height:")
+age = st.sidebar.number_input("Enter your age:", 0, 100, 0)
+
+if st.button("Predict"):
+    results = predict(
+        joined,
+        power_jumping,
+        power_stamina,
+        passing,
+        team_jersey_number,
+        defending,
+        shooting,
+        attacking_heading_accuracy,
+        height,
+        age,
+    )
+    st.success(results)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import joblib
+
+
+def predict(
+    joined,
+    power_jumping,
+    power_stamping,
+    passing,
+    team_jersey_number,
+    defending,
+    shooting,
+    attacking_heading_accuracy,
+    height_cm,
+    age,
+):
+    model = joblib.load(r"C:\Users\Tash-tech\OneDrive - Nelson Mandela University\Documents\FIFA\XGBoost.pkl")
+
+    test_data = [[
+        joined,
+        power_jumping,
+        power_stamping,
+        passing,
+        team_jersey_number,
+        defending,
+        shooting,
+        attacking_heading_accuracy,
+        height_cm,
+        age,
+    ]]
+    pred = model.predict(test_data)
+    return pred
+
+
+predict(45, 25, 12, 25, 12, 12, 15, 25, 25, 12)
